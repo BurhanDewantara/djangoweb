258aa0dbf65b8ae241dfa8037c24bd94e664f9cd
Burhan Dewantara <burhan.dewantara@gmail.com>
2017-12-24 01:39:45 +0700

    implement get string for album model

--- music/models.py
+++ music/models.py
@@ -7,6 +7,10 @@ class Album(models.Model):
     genre = models.CharField(max_length=100)
     logo = models.CharField(max_length=500)
 
+    def __str__ (self):
+        return self.artist + ' - ' + self.title
+
+
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
