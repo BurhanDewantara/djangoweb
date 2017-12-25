a616967047345906c2a8552a875e8b29356f0eb8
Burhan Dewantara <burhan.dewantara@gmail.com>
2017-12-25 14:58:59 +0700

    clean up views.py using render instead of template / http response

--- music/views.py
+++ music/views.py
@@ -1,17 +1,15 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Album
 
 # Create your views here.
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
     context = {
         'all_albums' : all_albums,
     }
-    
-    return HttpResponse(template.render(context,request))
+    return render(request, "music/index.html",context)
 
 def show_detail(request, album_id):
     return HttpResponse("music id = " + str(album_id))
