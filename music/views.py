07ff1496f8e64dc1bf685f8faf0ca9803951f924
Burhan Dewantara <burhan.dewantara@gmail.com>
2017-12-25 14:55:40 +0700

    clean up views.py and use template and context instead

--- music/views.py
+++ music/views.py
@@ -1,17 +1,17 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Album
 
 # Create your views here.
 
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
-
-    for album in all_albums:
-        url = "/music/" + str(album.id) +"/"
-        html += '<a href="'+url+'">'+album.title+'</a><br>'
-
-    return HttpResponse(html)
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums' : all_albums,
+    }
+    
+    return HttpResponse(template.render(context,request))
 
 def show_detail(request, album_id):
     return HttpResponse("music id = " + str(album_id))
