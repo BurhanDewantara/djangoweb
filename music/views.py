02c824fdc15a22f6acf9877283a40b10e9eb9a62
Burhan Dewantara <burhan.dewantara@gmail.com>
2017-12-26 00:22:25 +0700

    add shortcut for get object or 404

--- music/views.py
+++ music/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+
 from .models import Album
 
 # Create your views here.
@@ -10,12 +9,8 @@ def index(request):
     context = {
         'all_albums' : all_albums,
     }
-    return render(request, "music/index.html",context)
+    return render(request, "music/index.html", context)
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album Does no Exist")
-    
+    album = get_object_or_404(Album, pk=album_id)
     return render (request, "music/detail.html", {'album':album})
