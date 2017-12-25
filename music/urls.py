3b0fcdadd514f670d21340d761d538ad8a77428a
Burhan Dewantara <burhan.dewantara@gmail.com>
2017-12-25 14:35:19 +0700

    add music main page, and detail url

--- music/urls.py
+++ music/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index)
-
+    path('',views.index),
+    path('<int:album_id>/',views.show_detail,name="detail")
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,17 @@
 from django.http import HttpResponse
+from .models import Album
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hola") 
+    all_albums = Album.objects.all()
+    html = ''
+
+    for album in all_albums:
+        url = "/music/" + str(album.id) +"/"
+        html += '<a href="'+url+'">'+album.title+'</a><br>'
+
+    return HttpResponse(html)
+
+def show_detail(request, album_id):
+    return HttpResponse("music id = " + str(album_id))
