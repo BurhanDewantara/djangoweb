65e2c1df93aa2cafe58e05188bba1f59fe3551fc
Burhan Dewantara <burhan.dewantara@gmail.com>
2017-12-27 00:50:07 +0700

    add favorite function along with the form

--- music/views.py
+++ music/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Album
+from .models import Album, Song
 
 # Create your views here.
 
@@ -15,6 +15,17 @@ def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     return render (request, "music/detail.html", {'album':album})
 
-def favorite(request, album_id, song_id):
-    pass
-    
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    song_id = request.POST['song']
+    context = { 'album':album } 
+    try:
+        selected_song = album.song_set.get(pk=song_id)
+    except (KeyError, Song.DoesNotExist):
+        context['error_message'] = "Song Does Not Exist"
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+
+    return render (request, "music/detail.html", context )
+        
