c86fa224c406bef3a1e951e4bafedb34ec1449fc
Burhan Dewantara <burhan.dewantara@gmail.com>
2017-12-29 00:29:52 +0700

    complete create view for album

--- music/models.py
+++ music/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Album(models.Model):
@@ -6,6 +7,9 @@ class Album(models.Model):
     title = models.CharField(max_length=250)
     genre = models.CharField(max_length=100)
     logo = models.CharField(max_length=500)
+
+    def get_absolute_url(self):
+        return reverse('music:detail', kwargs={'pk': self.pk})
 
     def __str__ (self):
         return self.artist + ' - ' + self.title
